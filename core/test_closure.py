24fa8e1f798f4454daab201bdda4abd330ae3e36
Mohamed Merzoug <mohamed.merzoug.essbo@gmail.com>
2026-09-07 11:53:49 +0100

    Make closure tests independent of collected static assets

--- core/test_closure.py
+++ core/test_closure.py
@@ -18,6 +18,8 @@ from notifications.models import Notification
 
 
 @override_settings(PASSWORD_HASHERS=['django.contrib.auth.hashers.MD5PasswordHasher'],
+                   STORAGES={'default': {'BACKEND': 'django.core.files.storage.FileSystemStorage'},
+                             'staticfiles': {'BACKEND': 'django.contrib.staticfiles.storage.StaticFilesStorage'}},
                    PRIVILEGED_MFA_ENFORCEMENT=False, DOCUMENT_PDF_ENABLED=False)
 class ClosureTests(TestCase):
     def setUp(self):
